246f04daaf7d4af30dc499eb7cf54c825394f314
Bogdan Arabadzhi <bogdan.today@gmail.com>
2020-10-19 16:01:39 +0200

    Upgrade intersection-wasm

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,12 +1,15 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./bootstrap.js",
+  mode: "development",
+  experiments: {
+    asyncWebAssembly: true
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js"
   },
-  mode: "development",
-  plugins: [new CopyWebpackPlugin(["index.html"])]
+  plugins: [new CopyPlugin({ patterns: ["index.html"] })]
 };
